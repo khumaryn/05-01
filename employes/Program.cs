3b47e77383219697b9a88ee6df5a81928bd1f7d5
khumaryn <119412655+khumaryn@users.noreply.github.com>
2023-01-05 09:24:12 +0400

    Add files via upload

---
+++ employes/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace employes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iscilerin sayini daxil edin:");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Employes[] employees = new Employes[count];
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.Write($"{i + 1}. Fullaname:");
+                string fullname = Console.ReadLine();
+
+                Console.Write($"{i + 1}. NO:");
+                string no = Console.ReadLine();
+
+                Console.Write($"{i + 1}. Salary:");
+                int salary = Convert.ToInt32(Console.ReadLine());
+
+
+                string opt;
+                do
+                {
+                    Console.WriteLine("1. isciler uzerinde axtaris");
+                    Console.WriteLine("2. Yeni isci yarat");
+                    Console.WriteLine("0.  cixmaq");
+
+                    opt = Console.ReadLine();
+
+                    if (opt == "1")
+                    {
+                        Console.WriteLine("Axtaris deyerini daxil edin:");
+                        string search = Console.ReadLine();
+
+                        var filteredArr = SearchByName(employes, search);
+
+                        ShowEmployesInfo(filteredArr);
+                    }
+                    else if (opt == "2")
+                    {
+
+                        try
+                        {
+                            var employes = CreateEmployes();
+                            Array.Resize(ref employes, employes.Length + 1);
+                            employes[employes.Length - 1] = employes;
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Xeta bas verdi");
+                            Console.WriteLine("Xeta: " + e.Message);
+
+                        }
+                    }
+
+                } while (opt != "0");
+
+            }
+
+            static Employes[] SearchByName(Employes[] arr, string search)
+            {
+                Employes[] newArr = new Employes[0];
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i].name.Contains(search))
+                    {
+                        Array.Resize(ref newArr, newArr.Length + 1);
+                        newArr[newArr.Length - 1] = arr[i];
+                    }
+                }
+
+                return newArr;
+            }
+
+            static void ShowEmployesInfo(Employes[] arr)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[i].GetInfo());
+                }
+            }
+
+            static void Add(ref Employes[] arr, Employes value)
+            {
+                Employes[] newArr = new Employes[arr.Length + 1];
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    newArr[i] = arr[i];
+                }
+
+                newArr[newArr.Length - 1] = value;
+                arr = newArr;
+            }
+
+            static Employes CreateEmployes()
+            {
+                Console.WriteLine("Ad:");
+                string name = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(name) || name.Length < 3)
+                {
+                    throw new Exception("Ad-in uzunlugu minimum 3 olmalidir");
+                }
+
+                Console.WriteLine("Soyad:");
+                string surname = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(surname) || surname.Length < 3)
+                {
+                    throw new Exception("Soyadin-in uzunlugu minimum 3 olmalidir");
+                }
+
+                Employes employe = new Employes
+                {
+                    Name = name,
+                    Surname = surname
+                };
+
+                return employes;
+
+            }
+        }
+    }
+    
+
+
+
+
+
+
+
+}
+        }
+
+    }
+
+
+}
---
+++ employes/employes.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Xml.Linq;
+
+namespace employes
+{
+    internal class Employes
+    {
+        public string name;
+        public string surname;
+        public string _fullName;
+        public string No;
+        public int _salary ;
+        public Employes(string no, string fullname,int salary,)
+        {
+            No = no;
+            
+             Salary=salary;
+        }
+        public int Salary
+        {
+            get => _salary;
+            set
+            {
+                if (value >= 250)
+                {
+                    _salary = value;
+                }
+                else
+                {
+                    throw new Exception("Maas deyrei 250-dan kicik ola bilmez!");
+                }
+            }
+        }
+        static bool IsCapitalized(string str)
+        {
+            if (!Char.IsUpper(str[0]))
+                return false;
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (!Char.IsLower(str[i]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        static bool IsFullname(string str)
+        {
+            var words = str.Split(' ');
+
+            if (words.Length != 2)
+                return false;
+
+            if (!IsCapitalized(words[0]) || !IsCapitalized(words[1]))
+                return false;
+
+            return true;
+        }
+        static bool ContainsOnlyLetter(string str)
+        {
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (!char.IsLetter(str[i]))
+                        return false;
+                }
+                return true;
+            }
+            return false;
+        }
+        public string GetInfo()
+        {
+            return $"fullName: {_fullName} - Salary: {Salary}-No{No}";
+        }
+
+
+    }
+
+    
+
+
+    
+}
